34564854e3ecde7ffda8221ce5fc94e81632e155
MorveN11 <morales.patty.jose@gmail.com>
2024-03-01 00:03:22 -0400

    improve test

--- library/ManuelMoralesTest.hs
+++ library/ManuelMoralesTest.hs
@@ -1,7 +1,14 @@
 {-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
 
 {-# HLINT ignore "Use foldr" #-}
-module ManuelMoralesTest where
+module ManuelMoralesTest
+  ( average,
+    stringToListStudents,
+    stringToListStudentNames,
+    ageLessThan,
+    ageBetweenThan,
+  )
+where
 
 type Student = (String, Int)
 
@@ -15,54 +22,52 @@ isNotSpace x
   | x /= ' ' = True
   | otherwise = False
 
-isComma :: Char -> Bool
-isComma x
-  | x == ',' = True
-  | otherwise = False
-
 isNotComma :: Char -> Bool
 isNotComma x
   | x /= ',' = True
-  | otherwise = False
-
-isSpaceOrComma :: Char -> Bool
-isSpaceOrComma x
-  | x == ' ' = True
-  | x == ',' = True
   | otherwise = False
 
 sumAll :: [Student] -> Int
 sumAll [] = 0
 sumAll (x : xs) = snd x + sumAll xs
 
-average :: [Student] -> Int
+average :: [Student] -> Float
 average [] = 0
-average xs = sumAll xs `div` length xs
+average xs = fromIntegral (sumAll xs) / fromIntegral (length xs)
 
-strName :: String -> [Student]
-strName xs = (name, age) : strName next
+-- Input: "Ana 25, Jose 37, Maria 23, John 33"
+-- Output: ["Ana","Jose","Maria","John"]
+stringToListStudentNames :: String -> [String]
+stringToListStudentNames [] = []
+stringToListStudentNames xs = name : stringToListStudentNames next
   where
     name = takeWhile isNotSpace xs
-    rest = dropWhile isSpace name
+    rest = dropWhile isSpace (dropWhile isNotSpace xs)
+    next = dropWhile isSpace (dropWhile isNotSpace rest)
+
+-- Input: "Ana 25, Jose 37, Maria 23, John 33"
+-- Output: [("Ana",25),("Jose",37),("Maria",23),("John",33)]
+stringToListStudents :: String -> [Student]
+stringToListStudents [] = []
+stringToListStudents xs = (name, age) : stringToListStudents next
+  where
+    name = takeWhile isNotSpace xs
+    rest = dropWhile isSpace (dropWhile isNotSpace xs)
     age = read (takeWhile isNotComma rest)
-    next = dropWhile isSpaceOrComma rest
+    next = dropWhile isSpace (dropWhile isNotSpace rest)
 
 ageLessThan :: [Student] -> Int -> [Student]
 ageLessThan [] _ = []
-ageLessThan (x : xs) less
-  | null xs = [x]
-  | otherwise =
-    do
-      if snd x < less
-        then x : ageLessThan xs less
-        else ageLessThan xs less
+ageLessThan (x : xs) less =
+  do
+    if snd x < less
+      then x : ageLessThan xs less
+      else ageLessThan xs less
 
-ageLessThanMajorThan :: [Student] -> Int -> Int -> [Student]
-ageLessThanMajorThan [] _ _ = []
-ageLessThanMajorThan (x : xs) less major
-  | null xs = [x]
-  | otherwise =
-    do
-      if snd x >= less && snd x <= major
-        then x : ageLessThan xs less
-        else ageLessThan xs less
+ageBetweenThan :: [Student] -> Int -> Int -> [Student]
+ageBetweenThan [] _ _ = []
+ageBetweenThan (x : xs) less major =
+  do
+    if snd x >= less && snd x <= major
+      then x : ageBetweenThan xs less major
+      else ageBetweenThan xs less major
